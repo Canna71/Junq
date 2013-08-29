00147bd32425823ccd396849afd7d24afeb1afb6
Canna71 <gcannata@gmail.com>
2013-08-29 21:14:06 +0200

    Junq v. 1.0

---
+++ junq.js
@@ -0,0 +1,349 @@
+﻿/* Junq library v 1.0.0.0
+Copyright Gabriele Cannata 2013
+ */
+
+var junq = (function (undefined) {
+
+    var identity = function (o) { return o; };
+    var trueConstant = function () { return true; };
+    var falseConstant = function () { return false; };
+    var stdcomparison = function (a, b) { return a < b; };
+    var equality = function (a, b) { return a === b; };
+    var doNothing = function(){};
+    var emptyenum = { moveNext: falseConstant, getCurrent: doNothing };
+
+    //junq.prototype.constructor = junq;
+    var wrapper = function Junqy(o) {
+        this._o = o;
+        //not really needed
+        return this;
+    };
+
+   
+
+    var junq = function (o) {
+        return new wrapper(o);
+    };
+
+
+
+    wrapper.prototype.getEnumerator = function () {
+        return junq.enumerate(this._o);
+    };
+
+    wrapper.prototype.forEach = function (func) {
+        junq.forEach(this, func);
+    };
+    wrapper.prototype.filter = wrapper.prototype.where = function (func) {
+        return junq(junq.filter(this, func));
+    };
+
+    wrapper.prototype.toArray = function () {
+        return junq.toArray(this);
+    };
+    wrapper.prototype.select = wrapper.prototype.map = function (func) {
+        return junq(junq.select(this, func));
+    };
+
+    wrapper.prototype.flatmap = function (func) {
+        return junq(junq.flatmap(this,func));
+    };
+
+    wrapper.prototype.flatten = function () {
+        return junq(junq.flatten(this));
+    };
+
+    wrapper.prototype.append = function (o) {
+        return junq(junq.append(junq(o)).to(this));
+    };
+
+
+
+
+    wrapper.prototype.first = function (f) {
+        return junq.first(this, f);
+    };
+
+    wrapper.prototype.any = function (f) {
+        return junq.any(this, f);
+    };
+
+
+    wrapper.prototype.min = function (comparison) {
+        return junq.min(this, comparison);
+    };
+
+    wrapper.prototype.max = function (comparison) {
+        return junq.max(this, comparison);
+    };
+    wrapper.prototype.aggregate = function (aggregator, initial) {
+        return junq.aggregate(this, aggregator, initial);
+    };
+
+    wrapper.prototype.contains = function (val, eq) {
+        return junq.contains(this, val, eq);
+    };
+
+    //junq.prototype.init.prototype = junq.prototype;
+
+    //static methods 
+
+    junq.enumerate = function (o) {
+
+        if (o === undefined) {
+            throw new Error('Cannot enumerate undefined!');
+        }
+
+        if (o.getEnumerator) {//consider this an enumerable
+            return o.getEnumerator();
+        }
+        /** TODO other special cases **/
+        if (o.moveNext && o.getCurrent) {//consider this an enumerator
+            return o;
+        }
+
+
+        if (Array.isArray(o) || o.hasOwnProperty('length')) {
+            return new junq.ArrayEnumerator(o);
+        }
+
+
+        if (arguments.length) {
+            return junq.enumerate(arguments);
+        }
+
+        throw new Error('Cannot enumerate ' + o);
+    };
+    junq.forEach = function forEach(enumerable, func) {
+        var enumerator = junq.enumerate(enumerable);
+        while (enumerator.moveNext()) {
+            func(enumerator.getCurrent());
+        }
+    };
+
+
+    junq.filter = junq.where = function filter(enumerable, predicate) {
+        var e = junq.enumerate(enumerable);
+
+        return ({
+            moveNext: function () {
+                while (e.moveNext()) {
+                    if (predicate(e.getCurrent())) {
+                        return true;
+                    }
+                }
+                return false;
+            },
+            getCurrent: function () {
+                return e.getCurrent();
+            }
+        });
+    };
+
+    junq.select = junq.map = function select(enumerable, func) {
+        var enumerator = junq.enumerate(enumerable);
+        return ({
+            moveNext: function () {
+                return enumerator.moveNext();
+            },
+            getCurrent: function () {
+                return func(enumerator.getCurrent());
+            }
+        });
+
+    };
+
+    junq.flatmap = function (enumerable, func) {
+        var outer = junq.enumerate(enumerable);
+        var f = func || identity;
+        var lastres;
+        var currentouter;
+        return ({
+            moveNext: function () {
+                do {
+                    if (!lastres) {
+                        if (!(lastres = outer.moveNext())) return false;
+                        currentouter = junq.enumerate(f(outer.getCurrent()));
+                    }
+                    if (lastres = currentouter.moveNext())
+                        return true;
+                }
+                while (!lastres);
+                return false;
+            },
+            getCurrent: function () {
+                return currentouter.getCurrent();
+            }
+        });
+
+    };
+
+    junq.flatten = function (enumerable, func) {
+        var outer = junq.enumerate(enumerable);
+        var f = func || identity;
+        var lastres;
+        var currentouter;
+        return ({
+            moveNext: function () {
+                do {
+                    if (!lastres) {
+                        if (!(lastres = outer.moveNext())) return false;
+                        currentouter = junq.enumerate(outer.getCurrent());
+                    }
+                    if (lastres = currentouter.moveNext())
+                        return true;
+                }
+                while (!lastres);
+                return false;
+            },
+            getCurrent: function () {
+                return f(currentouter.getCurrent());
+            }
+        });
+
+    };
+
+    junq.toArray = function (enumerable) {
+        var a = [];
+        junq.forEach(enumerable, function (o) {
+            a.push(o);
+        });
+        return a;
+    };
+
+    junq.range = function (length, start, step) {
+        return junq({
+            getEnumerator: function () {
+                return new junq.RangeEnumerator(length, start, step);
+            }
+        });
+    };
+
+    junq.append = function (enumerable) {
+        return {
+            to: function (other) {
+                return junq.concat(other, enumerable);
+            }
+        };
+
+    };
+
+
+    junq.concat = function (first, then) {
+        return junq.flatten(junq.enumerate([first, then]));
+    };
+
+    junq.first = function (enumerable, predicate) {
+        var f = predicate || trueConstant;
+        var e = junq.enumerate(enumerable);
+        while (e.moveNext()) {
+            if (f(e.getCurrent())) {
+                return e.getCurrent();
+            }
+        }
+        return undefined;
+    };
+
+    junq.any = function (enumerable, predicate) {
+        return junq.first(enumerable, predicate) !== undefined;
+    };
+
+    junq.min = function (enumerable, comparison) {
+        var c = comparison || stdcomparison;
+
+        return junq.aggregate(enumerable, function (acc, x) {
+            return acc && c(acc, x) ? acc : x;
+        });
+    };
+
+    junq.max = function (enumerable, comparison) {
+        var c = comparison || stdcomparison;
+
+        return junq.aggregate(enumerable, function (acc, x) {
+            return !acc || c(acc, x) ? x : acc;
+        });
+    };
+
+    junq.aggregate = function (enumerable, agg, initial) {
+        var acc = initial;
+        junq.forEach(enumerable, function (x) {
+            acc = agg(acc, x);
+        });
+        return acc;
+    };
+
+    junq.contains = function (enumerable, o, eq) {
+        eq = eq || equality;
+        return junq.any(enumerable, function (x) {
+            return eq(o, x);
+        });
+    };
+
+    junq.repeat = function (element, count) {
+        return junq({
+            getCurrent: function () {
+                return element;
+            },
+            moveNext: function () {
+                return (count--) > 0;
+            }
+        });
+    };
+
+    //nested "classes"
+    junq.ArrayEnumerator = (function () {
+        function ArrayEnumerator(array) {
+            this._array = array;
+            this._current = -1;
+        }
+        ArrayEnumerator.prototype.moveNext = function () {
+            this._current++;
+            return this._current < this._array.length;
+
+        };
+
+        ArrayEnumerator.prototype.getCurrent = function () {
+            return this._array[this._current];
+        };
+        return ArrayEnumerator;
+    })();
+
+    junq.RangeEnumerator = (function () {
+        function RangeEnumerator(length, start, step) {
+            if (start === undefined) {
+                start = 0;
+            }
+
+            if (step === undefined) {
+                step = 1;
+            }
+            if (length === undefined) {
+                throw new Error('length must be speccified');
+            }
+            this._current = start-step; //is incremented in the first moveNext
+            this._length = length;
+            this._step = step;
+        }
+        RangeEnumerator.prototype.moveNext = function () {
+            
+            if (this._length > 0) {
+                this._length--;
+                this._current += this._step;
+                return true;
+            }
+            return false;
+        };
+
+        RangeEnumerator.prototype.getCurrent = function () {
+            return this._current;
+        };
+        return RangeEnumerator;
+    })();
+
+    return junq;
+})();
+
+if (typeof(module) !== 'undefined') { module.exports = junq; }
+
+
+
---
+++ sets.js
@@ -0,0 +1,166 @@
+﻿/* Junq library v 1.0.0.0
+ Copyright Gabriele Cannata 2013
+ Sets extensions
+ */
+
+var junq = junq || require('./junq.js');
+
+var sets;
+(function (sets, undefined) {
+
+
+    var Set = (function () {
+
+        var internalRep = function (objects) {
+            var self = this;
+            self.set = [];
+            junq(objects).forEach(function (o) {
+                self.add(o);
+
+            });
+
+        };
+
+        function Set(objects) {
+            if (objects !== undefined) {
+                if (arguments.length == 1)
+                    internalRep.call(this, objects);
+                else
+                    internalRep.call(this, arguments);
+            } else {
+                internalRep.call(this, []);
+            }
+
+        }
+
+        var isSet = function isSet(o) {
+            if (o !== undefined) {
+                return o.constructor === Set;
+            } else {
+                return false;
+            }
+
+        };
+        sets.isSet = isSet;
+
+
+        Set.prototype.toString = function () {
+
+            return '{' + this.toArray().join(', ') + '}';
+        };
+
+        Set.prototype.forEach = function (func) {
+            this.set.forEach(func);
+            return this;
+        };
+        //TODO: try indexOf
+        Set.prototype.contains = function (element) {
+            return (this.set.indexOf(element) >= 0);
+            // var l=this.set.length
+            // for(var i=0; i<l;i++){
+            //     if(this.set[i]===element) return true;
+            // }
+            // return false;
+        };
+
+        Set.prototype.add = function (element) {
+            if (!this.contains(element))
+                this.set.push(element);
+            return this;
+        };
+
+        Set.prototype.remove = function (element) {
+            var l = this.set.length;
+            for (var i = 0; i < l; i++) {
+                if (this.set[i] === element) {
+                    this.set.splice(i, 1);
+                    break;
+                }
+            }
+            return this;
+        };
+
+        Set.prototype.union = function (other) {
+
+            var res = this.clone();
+            if (isSet(other)) {
+                other.forEach(function (e) {
+                    res.add(e);
+                });
+            } else {
+                res.add(other);
+            }
+
+            return res;
+        };
+
+        Set.prototype.clone = function () {
+            var res = new Set();
+            res.set = this.set.slice(0, this.set.length);
+            return res;
+        };
+
+        Set.prototype.intersect = function (other) {
+            var res = new Set();
+            this.forEach(function (e) {
+                if (other.contains(e))
+                    res.add(e);
+            });
+
+            return res;
+        };
+
+        Set.prototype.cardinality = function () {
+            return this.set.length;
+        };
+
+        Set.prototype.subtract = function (other) {
+
+            var res = this.clone();
+
+            other.forEach(function (e) {
+                res.remove(e);
+            });
+            return res;
+        };
+
+        Set.prototype.equalTo = function (other) {
+            if (this.cardinality() !== other.cardinality()) return false;
+            return this.subtract(other).isEmpty();// && other.subtract(this).isEmpty();
+
+        };
+
+        Set.prototype.isEmpty = function () {
+            return this.set.length === 0;
+
+        };
+
+        Set.prototype.getEnumerator = function () {
+            return junq.enumerate(this.toArray());
+        };
+
+        Set.prototype.toArray = function () {
+
+            return this.set;
+        };
+
+        Set.prototype.isSingleton = function () {
+            return this.set.length === 1;
+        };
+
+        Set.prototype.getElement = function (n) {
+            n = n | 0;
+            if (this.set.length <= n)
+                throw new Error('Not enough elements!');
+            return this.set[n];
+        };
+
+        Set.prototype.getEnumerator = function () {
+            return new junq.ArrayEnumerator(this.set);
+        };
+
+        return Set;
+    })();
+    sets.Set = Set;
+
+})(sets || (sets = {}));
